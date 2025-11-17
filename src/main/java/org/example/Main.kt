d425d440cb7c5616063d746d746ffa7995a62f18
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-11-17 08:58:08 -0500

    created dog and objects

--- src/main/java/org/example/Main.kt
+++ src/main/java/org/example/Main.kt
@@ -4,4 +4,18 @@ package org.example
 
 fun main() {
     println("Welcome to the HouseholdPets Application")
+     val myCat = Cat(name= "Garfiled", age= 8)
+
+    myCat.info()
+    myCat.makeSound()
+    myCat.scratch()
+
+    println()
+
+    val myDog = Dog(name = "Odie", age = 5)
+
+
+    myDog.info()
+    myDog.makeSound()
+    myDog.fetch()
 }
